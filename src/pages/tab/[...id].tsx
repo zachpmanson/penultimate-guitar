f9342fe4ca56ef0a66dfc3e1f8cb176d105f24b7
Zach Manson <zachpmanson@gmail.com>
2023-02-12 12:59:03 +0800

    Max text size dynamic

--- src/pages/tab/[...id].tsx
+++ src/pages/tab/[...id].tsx
@@ -1,5 +1,6 @@
 import Chord from "@/components/chord";
 import SearchBox from "@/components/searchbox";
+import useWindowDimensions from "@/hooks/windowdimensions";
 import { TabDto } from "@/models";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -10,38 +11,58 @@ export default function Tab() {
   const router = useRouter();
   const { id } = router.query;
   const [tab, setTab] = useState(<></>);
+  const [plainTab, setPlainTab] = useState("");
   const [name, setName] = useState("");
   const [artist, setArtist] = useState("");
+
+  const { height, width } = useWindowDimensions();
   useEffect(() => {
-    if (typeof id === "object") {
-      fetch("/api/tab", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id: id.join("/") }),
-      })
-        .then((res) => res.json())
-        .then((res: TabDto) => {
-          console.log(res);
-          setTab(processTab(res.tab));
-          setName(res.name);
-          setArtist(res.artist);
-        });
-    }
+    if (typeof id !== "object") return;
+
+    fetch("/api/tab", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id.join("/") }),
+    })
+      .then((res) => res.json())
+      .then((res: TabDto) => {
+        console.log(res);
+        setPlainTab(res.tab);
+        setName(res.name);
+        setArtist(res.artist);
+      });
   }, [id]);
 
-  const processTab = (tabString: string) => {
+  useEffect(() => {
+    const maxLineLen = Math.max(
+      ...plainTab.split("\n").map((l: string) => l.length)
+    );
+
+    setTab(processTab(plainTab, maxLineLen));
+  }, [plainTab, width]);
+
+  const processTab = (tabString: string, maxLineLen: number) => {
     let cleanString: string = tabString
       .replace(/\[tab\]/g, "")
       .replace(/\[\/tab\]/g, "");
     console.log("cleanString", cleanString);
     let tabHtml = (
-      <pre>
-        {reactStringReplace(cleanString, /\[ch\](.+?)\[\/ch\]/g, (match, i) => (
-          <Chord chord={match} id={i} />
-        ))}
-      </pre>
+      <div
+        className="tab m-auto w-fit"
+        style={{ fontSize: Math.min(14, (1.5 * width) / maxLineLen) }}
+      >
+        <pre>
+          {reactStringReplace(
+            cleanString,
+            /\[ch\](.+?)\[\/ch\]/g,
+            (match, i) => (
+              <Chord chord={match} id={i} />
+            )
+          )}
+        </pre>
+      </div>
     );
     return tabHtml;
   };
