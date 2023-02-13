2443aa9c2f282238b11948e4a7d0be5b8fec7d77
Zach Manson <zachpmanson@gmail.com>
2023-02-13 15:03:38 +0800

    Change to global tooltip provider

--- src/components/chord.tsx
+++ src/components/chord.tsx
@@ -1,4 +1,4 @@
-import { Tooltip } from "react-tooltip";
+import { TooltipWrapper } from 'react-tooltip';
 import "react-tooltip/dist/react-tooltip.css";
 
 type ChordProps = {
@@ -9,13 +9,13 @@ export default function Chord({ chord, id }: ChordProps) {
   console.log("chord",chord, "id",id)
   return (
     <>
-      <span
-        id={`${id}-tooltip`}
-        className="text-blue-500"
-      >
-        {chord}
-      </span>
-      <Tooltip anchorId={`${id}-tooltip`} content="{`${chord} Chord tooltip here`}" clickable />
+      <TooltipWrapper content={`${chord} Chord tooltip here`}>
+        <span
+          className="text-blue-500"
+          >
+          {chord}
+        </span>
+      </TooltipWrapper>
     </>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { TooltipProvider, Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <TooltipProvider>
+      <Component {...pageProps} />
+      <Tooltip id="chord-tooltip" />
+    </TooltipProvider>
+  )
 }
