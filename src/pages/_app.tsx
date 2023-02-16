56827d533ea99e4f79d916c59e6fc0ccb3ad18fe
Zach Manson <zachpmanson@gmail.com>
2023-02-16 22:11:38 +0800

    Add favicons and manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,6 +28,44 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
               name="viewport"
               content="width=device-width, initial-scale=1.0"
             />
+            <meta
+              name="description"
+              content="Find the cheapest drinks, per standard"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/icons/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/icons/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/icons/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/icons/site.webmanifest" />
+            <link
+              rel="mask-icon"
+              href="/icons/safari-pinned-tab.svg"
+              color="#bd93f9"
+            />
+            <link rel="shortcut icon" href="/icons/favicon.ico" />
+            <meta name="msapplication-TileColor" content="#603cba" />
+            <meta
+              name="msapplication-config"
+              content="/icons/browserconfig.xml"
+            />
+            <meta name="theme-color" content="#282A36" />
+            <meta
+              name="description"
+              content="An alternate frontend for Ultimate Guitar"
+            />
           </Head>
           <Component {...pageProps} />
         </Layout>
