4961b8278c2989a5f4d441f62683d3bb4bc731ca
Zach Manson <zachpmanson@gmail.com>
2023-06-04 11:15:23 +0800

    Move gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/router";
 import { ReactElement, ReactNode, useEffect } from "react";
 import NProgress from "nprogress";
 import "../styles/nprogress.css";
+import Script from "next/script";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -89,6 +90,19 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
           />
         </Head>
         <Component {...pageProps} />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2Q5B9DT8HJ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-2Q5B9DT8HJ');
+            `}
+        </Script>
       </Layout>
     </GlobalProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,21 +7,6 @@ export default function Document() {
       <body className="p-4">
         <Main />
         <NextScript />
-        <div className="container">
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-2Q5B9DT8HJ"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-2Q5B9DT8HJ');
-            `}
-          </Script>
-        </div>
       </body>
     </Html>
   );
