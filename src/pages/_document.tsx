df492c3f6ea67cdc36823abc5a211cd888669aa0
Zach Manson <zachpmanson@gmail.com>
2023-06-04 11:04:23 +0800

    Add gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,21 @@ export default function Document() {
       <body className="p-4">
         <Main />
         <NextScript />
+        <div className="container">
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-2Q5B9DT8HJ"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-2Q5B9DT8HJ');
+            `}
+          </Script>
+        </div>
       </body>
     </Html>
   );
