27b607611e273432c85e47d9efc60cd6813dfa3a
Zach Manson <zachpmanson@gmail.com>
2023-06-07 20:09:20 +0800

    Add more options

--- science/poll/poll.sh
+++ science/poll/poll.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# arg 1 filename, arg 2 min time, arg 3 + arg2 = max time
+
 FILENAME=$1
+echo "starting at $STARTLINE"
 i=0
 
 start=$(date +%s)
@@ -8,10 +11,15 @@ start=$(date +%s)
 while read line; do
     cleanline=$(echo "$line" | tr -d '"')
     url="http://localhost:3000/tab/$cleanline/"
-    sleeptime=$(($RANDOM % 20 + 5))
+    sleeptime=$(($RANDOM % ${4:-20} + ${3:-5}))
 
-    echo "line:$((++i)) sleep:$sleeptime url:$cleanline"
-    curl -o /dev/null -s -w "%{http_code}\n" -L "$url"
+    echo -e "#$((++i))\turl:$cleanline"
+    if [[ $i -lt ${2:-1} ]]; then
+        continue
+    fi
+
+    curl -o /dev/null -s -w "\t%{http_code} %{time_total}sec\n" -L "$url"
+    echo -e "\tsleep $sleeptime" "secs"
     sleep $sleeptime
 done <"$FILENAME"
 
