0b224a2dcf33895227ba87a71c184ec3074db5e3
Zach Manson <zachpmanson@gmail.com>
2024-02-19 09:55:41 +1100

    Readd google analytics (temporarily)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -94,6 +94,18 @@ export default function App({
             />
           </Head>
           <Component {...pageProps} />
+          <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2Q5B9DT8HJ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2Q5B9DT8HJ');
+            `}
+        </Script>
         </Layout>
       </GlobalProvider>
     </SessionProvider>
