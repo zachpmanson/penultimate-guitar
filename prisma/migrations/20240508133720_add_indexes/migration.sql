f50d22abdd7603e063bd1e78d4cba837e04a21e7
Zach Manson <zachpmanson@gmail.com>
2024-05-08 23:37:38 +1000

    Add index

---
+++ prisma/migrations/20240508133720_add_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Tab_songId_idx" ON "Tab"("songId");
+
+-- CreateIndex
+CREATE INDEX "Tab_taburl_idx" ON "Tab"("taburl");
+
+-- CreateIndex
+CREATE INDEX "User_spotifyUserId_idx" ON "User"("spotifyUserId");
