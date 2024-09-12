0518e0ea9ae572b0504e93199a12ccafa6176234
Zach Manson <zachpmanson@gmail.com>
2024-09-12 22:05:30 +1000

    Fix menu z index

--- src/components/home/savedtabs.tsx
+++ src/components/home/savedtabs.tsx
@@ -194,7 +194,7 @@ function FolderMenu({ folder }: { folder: Folder }) {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white dark:bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="z-10 absolute right-0 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white dark:bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
               {folder.playlistUrl && (
                 <>
