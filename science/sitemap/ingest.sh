0f118703ef009b2199a32d29c827f78e29fb734c
Zach Manson <zachpmanson@gmail.com>
2024-09-16 20:12:32 +1000

    print filename

--- science/sitemap/ingest.sh
+++ science/sitemap/ingest.sh
@@ -6,8 +6,9 @@ set -e
 
 function read_children() {
     while read p; do
-        echo "$p"
-        curl -s "$p" > "autorefresh/$(grep -e "sitemap[0-9]+" -e "sitemap_ukulele" -o <<< "$p").xml"
+        OUTFILE="autorefresh/$(grep -e "sitemap[0-9]+" -e "sitemap_ukulele" -o <<< "$p").xml"
+        echo "$p" ">" "$OUTFILE"
+        curl -s "$p" > $OUTFILE
         # curl -s "$p" > "autorefresh/$(date -I)-$(grep -e "sitemap[0-9]" -e "sitemap_ukulele" -o <<< "$p").xml"
         sleep 10 
     done <autorefresh/sitemaps.txt
