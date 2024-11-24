e694d88bbafa4cb4852ae61c3b92e775d941e3e5
Zach Manson <zachpmanson@gmail.com>
2024-11-24 21:42:19 +1100

    Update filename for csv

--- science/sitemap/update_possible_songs.sql
+++ science/sitemap/update_possible_songs.sql
@@ -3,7 +3,7 @@ CREATE TEMPORARY TABLE temp_possible_songs AS
 WITH NO DATA;
 
 -- \copy temp_possible_songs (name,artist,taburl,type,"originalId") FROM '/Users/zach/Documents/penultimate-guitar/science/sitemap/autorefresh/tab-urls-with-id.csv' DELIMITER ',' csv header;
-\copy temp_possible_songs (name,artist,taburl,type) FROM '/root/penultimate-guitar/science/sitemap/autorefresh/tab-urls.csv' DELIMITER ',' csv header;
+\copy temp_possible_songs (name,artist,taburl,type) FROM '/root/penultimate-guitar/science/sitemap/autorefresh/tab-urls-with-id.csv' DELIMITER ',' csv header;
 
 INSERT INTO public."PossibleSong" (name,artist,taburl,type,"originalId")
     SELECT name,artist,taburl,type,"originalId" FROM temp_possible_songs
