7f9aa041c62491f0bfd602e9197400fa160d920d
Zach Manson <zachpmanson@gmail.com>
2025-12-24 10:31:36 +0800

    Make routes consts

--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,9 @@ export const blacklist = ["Pro", "Video", "Official", "Power"];
 export const GuitaleleStyle = "text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600";
 
 export const ROUTES = {
-  TAB: (id: string) => `/tab/${id}`,
-  BEST_TAB: (id: string) => `/best/tab/${id}`,
-  ID: (id: string) => `/id/${id}`,
-  BEST_ID: (id: string) => `/best/id/${id}`,
-  TRACK: (id: string) => `/track/${id}`,
+  TAB: (id: string) => `/tab/${id}` as const,
+  BEST_TAB: (id: string) => `/best/tab/${id}` as const,
+  ID: (id: string) => `/id/${id}` as const,
+  BEST_ID: (id: string) => `/best/id/${id}` as const,
+  TRACK: (id: string) => `/track/${id}` as const,
 };
