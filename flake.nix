1107aea62b2b42c60b4a61005b4cd2842b091741
Zach Manson <zachpmanson@gmail.com>
2026-06-18 23:41:29 +1000

    nixpkgs follows set

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "penultimate-guitar";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
